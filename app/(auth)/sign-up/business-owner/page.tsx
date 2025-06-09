c8e954cb105a6b1ec20969c8adb17777f5818086
Rydhlnst <rydhlnst@gmail.com>
2025-06-09 15:17:08 +0700

    React Component fix

---
+++ app/(auth)/sign-up/business-owner/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUpBusinessOwner = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
+
+export default SignUpBusinessOwner
---
+++ app/(auth)/sign-up/choose-role/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChooseRole = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
+
+export default ChooseRole
